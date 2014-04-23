b1b90a5eb87bc7ec70424bd81ca9724a6c691143
Michel Hognerud <leyou.m@gmail.com>
2014-04-24 07:14:23 +0800

    fixes

--- asgard-install.php
+++ asgard-install.php
@@ -1,4 +1,6 @@
 <?php
-require_once __DIR__.'/../utils/FileManager.php';
+require_once 'paths.php';
+require _CORE_DIR_.'core.php';
+\Asgard\Core\App::loadDefaultApp();
 
-\Asgard\Utils\FileManager::copy(__DIR__.'/app/sitemaphtml', 'app/sitemaphtml');
+\Asgard\Utils\FileManager::copy(__DIR__.'/app/sitemaphtml', _DIR_.'app/sitemaphtml');
